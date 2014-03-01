34b52c9d67ba95081f571d2d1326a9bb7851fec0
Aaron Chung <aawchung@gmail.com>
2014-03-01 13:41:04 -0800

    lol

--- distributionInfo.php
+++ distributionInfo.php
@@ -1,6 +1,6 @@
 <?php
 	//echo "BLAH";
-	//include "Users.php";
+	include "Users.php";
 	//echo "BLAH";
 
 	
@@ -16,17 +16,20 @@
 	echo $west;
 	*/
 	
-	/*
-	$result = findDistribution( $south, $north, $east, $west );
-	echo $result;
+	$result = findDistribution( $south, $north, $west, $east );
+	//var_dump( $result);
 	
 	$data = array( "events" => array() );	
-	if( result != false ){
-		$event = array( "name" => "TEST", "lat" => ($north+$south)/2.0 , "lng" => ($east+$west)/2.0,
-						"address" => "TEST", "date" => "DATE", "start" => "5", "end" => "6" );
-		$data["events"][] = $event;
+	if( result != false ){	
+		foreach( $result as $event ){
+			$distro = distributorFindById($event[1]);
+			$data["events"][] = array( "name" => $distro[1], "lat" => $event[2] , "lng" => $event[3],
+						"address" => $event[4], "start" => $event[5], "end" => $event[6] );
+		}
 	}
-	*/
+
+	
+	/*
 	$data = array( "events" => array() );	
 	$event = array( "name" => "feedThePoor", "lat" => 37.3184130 , "lng" => -121.8686210,
 						"address" => "300-312 E Alma Ave San Jose, CA 95112", "start" => "2014-03-01 21:59:00", "end" => "2014-03-01 23:00:00" );
@@ -35,7 +38,7 @@
 	$event = array( "name" => "feedPoor", "lat" => 37.3229180 , "lng" => -121.8698220,
 						"address" => "1131 S 9th St San Jose, CA 95112", "start" => "2014-03-01 16:00:00", "end" => "2014-03-01 20:00:00" );
 	$data["events"][] = $event;
-	
+	*/
 	echo json_encode( $data );	
 	
 ?>

e5e90860639ca206cf5c3c7c16b5d38a69d02d41
Aaron Chung <aawchung@gmail.com>
2014-02-28 23:34:33 -0800

    fixed

--- distrobutionInfo.php
+++ distrobutionInfo.php
@@ -3,16 +3,7 @@
 	$lat = var_dump($_POST['lat']);
 	$lng = var_dump($_POST['lng']);
 	
-	$data = array( "events" => array() );
-	
-					events[i][0] = data["events"][i]["name"];
-					events[i][1] = data["events"][i]["lat"];
-					events[i][2] = data["events"][i]["lng"];
-					events[i][3] = data["events"][i]["address"];
-					events[i][4] = data["events"][i]["date"];
-					events[i][5] = data["events"][i]["start"];
-					events[i][6] = data["events"][i]["end"];
-	
+	$data = array( "events" => array() );	
 	
 	$event = array( "name" => "TEST", "lat" => lat , "lng" => lng,
 					"address" => "TEST", "date" => "DATE", "start" => "5", "end" => "6" );

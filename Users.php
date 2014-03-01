731521df399f9580e1d6cb70caa3385f04293a4d
bpyoung92 <brandon.p.young@gmail.com>
2014-03-01 03:24:40 -0800

    sql functions updated

--- Users.php
+++ Users.php
@@ -1,133 +1,163 @@
 <?php
 
-class Table_Users extends DB
+$Server="50i50.org";
+$Catalog="homeless";
+$User="jesus";
+$Password="ayala";
+
+$mysqli = new mysqli($Server,$User,$Password,$Catalog);
+
+if ($mysqli->errno) 
+    printf("Unable to connect to the database:<br /> %s",$mysqli->error); 
+
+
+//require '/var/www/html/vendor/autoload.php';
+
+function addDonor($username, $email, $password, $organization, $address, $phoneNumber, $type)
 {
-	private $tb = 'users';
-	function addUser($username, $email, $pass)
+	$geocoder = new \Geocoder\Geocoder();
+	$adapter  = new \Geocoder\HttpAdapter\CurlHttpAdapter();
+	$chain    = new \Geocoder\Provider\ChainProvider(array(
+		new \Geocoder\Provider\GeocoderCaProvider($adapter),
+		new \Geocoder\Provider\ArcGISOnlineProvider($adapter, 'USA', true),
+		new \Geocoder\Provider\OpenStreetMapsProvider($adapter),
+		new \Geocoder\Provider\GoogleMapsProvider($adapter, '', 'USA', true),
+	));
+	$geocoder->registerProvider($chain);
+
+	$username = mysql_real_escape_string($username);
+	$email = mysql_real_escape_string($email);
+	$password = mysql_real_escape_string($password);
+	$organization = mysql_real_escape_string($organization);
+	$address = mysql_real_escape_string($address);
+	$phoneNumber = mysql_real_escape_string($phoneNumber);
+	$type = mysql_real_escape_string($type);
+	
+	try
 	{
-		$query = "INSERT INTO " . $this->tb . "(username, email, password, create_date, visit_date, type)
-VALUE('" . $this->escape($username) . "', '" . $this->escape($email) . "', '" . $this->escape($pass) . "', NOW(), NOW(), 2)";
-		$res = mysql_query($query);
+		$geocode = $geocoder->geocode($address);
+	}
+	catch (Exception $e)
+	{
+		echo $e->getMessage();
+	}
+	$latitude = (float)$geocode->getLatitude();
+	$longitude = (float)$geocode->getLongitude();
+	
+	$query = "INSERT INTO donor (username, email, password, organization, address, phoneNumber, latitude, longitude, createDate, validated, type)
+VALUE('$username', '$email', '$password', '$organization', '$address', '$phoneNumber', '$latitude', '$longitude', NOW(), 0, $type)";
+	$res = $mysqli->query($query);
+	if ($res) {
+		return mysql_insert_id();
+	}
+	return false;
+}
 
-		if ($res) {
-			return mysql_insert_id();
-		}
+function addDistributor($username, $email, $password, $organization, $address, $phoneNumber)
+{
+	$geocoder = new \Geocoder\Geocoder();
+	$adapter  = new \Geocoder\HttpAdapter\CurlHttpAdapter();
+	$chain    = new \Geocoder\Provider\ChainProvider(array(
+		new \Geocoder\Provider\GeocoderCaProvider($adapter),
+		new \Geocoder\Provider\ArcGISOnlineProvider($adapter, 'USA', true),
+		new \Geocoder\Provider\OpenStreetMapsProvider($adapter),
+		new \Geocoder\Provider\GoogleMapsProvider($adapter, '', 'USA', true),
+	));
+	$geocoder->registerProvider($chain);
 
-		return false;
+	$username = mysql_real_escape_string($username);
+	$email = mysql_real_escape_string($email);
+	$password = mysql_real_escape_string($password);
+	$organization = mysql_real_escape_string($organization);
+	$address = mysql_real_escape_string($address);
+	$phoneNumber = mysql_real_escape_string($phoneNumber);
+	
+	try
+	{
+		$geocode = $geocoder->geocode($address);
+	}
+	catch (Exception $e)
+	{
+		echo $e->getMessage();
+	}
+	$latitude = (float)$geocode->getLatitude();
+	$longitude = (float)$geocode->getLongitude();
+	
+	$query = "INSERT INTO donor (username, email, password, organization, address, phoneNumber, latitude, longitude, createDate, validated)
+VALUE('$username', '$email', '$password', '$organization', '$address', '$phoneNumber', '$latitude', '$longitude', NOW(), 0)";
+	$res = $mysqli->query($query);
+	if ($res) {
+		return mysql_insert_id();
+	}
+	return false;
+}
+
+function donorLogin($username, $password)
+{
+	$username = mysql_real_escape_string($username);
+	$password = mysql_real_escape_string($password);
+	$query = "SELECT id, username, organization FROM donor WHERE `username` = '$username AND `password` = '$password' LIMIT 1";
+	$res = $mysqli->query($query);
+	if (mysql_num_rows($res)) {
+		return mysql_fetch_assoc($res);
+	}
+	return false;
+}
+
+function donorFindById($id)
+{
+	$query = "SELECT id, username, organization FROM donor WHERE `id` = '$id' LIMIT 1";
+	$res = $mysqli->query($query);
+
+	if (mysql_num_rows($res)) {
+		return mysql_fetch_assoc($res);
 	}
 
-	function updateUserInfo($array, $id)
-	{
-		$update = array();
-		foreach ($array as $key => $val) {
-			$val = $this->escape($val);
-			$update[] = "`$key` = '$val'";
-		}
+	return false;
+}
 
-		$query ="UPDATE " . $this->tb . " SET " . implode(',', $update) . " WHERE id = " . intval($id);
-		return mysql_query($query);
+function distributorLogin($username, $password)
+{
+	$username = mysql_real_escape_string($username);
+	$password = mysql_real_escape_string($password);
+	$query = "SELECT id, username, organization FROM distributor WHERE `username` = '$username AND `password` = '$password' LIMIT 1";
+	$res = $mysqli->query($query);
+	if (mysql_num_rows($res)) {
+		return mysql_fetch_assoc($res);
+	}
+	return false;
+}
+
+function distributorFindById($id)
+{
+	$query = "SELECT id, username, organization FROM distributor WHERE `id` = '$id' LIMIT 1";
+	$res = $mysqli->query($query);
+
+	if (mysql_num_rows($res)) {
+		return mysql_fetch_assoc($res);
 	}
 
-	function delUser($id)
-	{
-		$query = 'DELETE FROM ' . $this->tb . ' WHERE id=' . intval($id);
-		return mysql_query($query);
+	return false;
+}
+
+function findDelivery($minLat, $maxLat, $minLon, $maxLon)
+{
+	$query = "SELECT * FROM deliveries WHERE `validated` = 0 AND $minLat < `latitude` AND $maxLat > `latitude` AND $minLon < `longitude` AND $maxLon > `longitude'";
+	$res = $mysqli->query($query);
+	
+	if (mysql_num_rows($res)) {
+		$info = mysqli_fetch_array($data);
+		return $info;
 	}
+}
 
-	function getUserForLogin($login, $pass)
-	{
-		$login = $this->escape($login);
-
-		$query = "SELECT id, username, email, type, lang FROM " . $this->tb . " WHERE (username='" . $login . "' OR email='" . $login . "') AND password='" . $this->pass($pass) . "' LIMIT 1";
-		$res = mysql_query($query);
-
-		if (mysql_num_rows($res)) {
-			return mysql_fetch_assoc($res);
-		}
-
-		return false;
+function findDistribution($minLat, $maxLat, $minLon, $maxLon)
+{
+	$query = "SELECT * FROM distributions WHERE `validated` = 0 AND $minLat < `latitude` AND $maxLat > `latitude` AND $minLon < `longitude` AND $maxLon > `longitude'";
+	$res = $mysqli->query($query);
+	
+	if (mysql_num_rows($res)) {
+		$info = mysqli_fetch_array($data);
+		return $info;
 	}
-
-	function getUserById($id)
-	{
-		$query = "SELECT id, username, email, type, lang FROM " . $this->tb . " WHERE id=" . intval($id) . " LIMIT 1";
-		$res = mysql_query($query);
-
-		if (mysql_num_rows($res)) {
-			return mysql_fetch_assoc($res);
-		}
-
-		return false;
-	}
-
-	function getUserByUsername($username)
-	{
-		$query = "SELECT id, username, email, type, lang FROM " . $this->tb . " WHERE username='" . $username . "' LIMIT 1";
-		$res = mysql_query($query);
-
-		if (mysql_num_rows($res)) {
-			return mysql_fetch_assoc($res);
-		}
-
-		return false;
-	}
-
-
-	function getUserByEmail($email)
-	{
-		$query = "SELECT id, username, email, type, lang FROM " . $this->tb . " WHERE email='" . $email . "' LIMIT 1";
-		$res = mysql_query($query);
-
-		if (mysql_num_rows($res)) {
-			return mysql_fetch_assoc($res);
-		}
-
-		return false;
-	}
-
-	function getUserByUserKey($username, $key)
-	{
-		$query = "SELECT id, username, email, type, lang FROM " . $this->tb . " WHERE username='" . $this->escape($username) . "' AND visit_key='" . $this->escape($key) . "' LIMIT 1";
-		$res = mysql_query($query);
-
-		if (mysql_num_rows($res)) {
-			return mysql_fetch_assoc($res);
-		}
-
-		return false;
-	}
-
-	function getUserByLoginOrEmail($login=null, $email=null)
-	{
-		if (empty($email)) {
-			$email = $login;
-		}
-		$login = $this->escape($login);
-
-		$query = "SELECT id, username, email, type, lang FROM " . $this->tb . " WHERE username='" . $login . "' OR email='" . $email . "' LIMIT 1";
-		$res = mysql_query($query);
-
-		if (mysql_num_rows($res)) {
-			return mysql_fetch_assoc($res);
-		}
-
-		return null;
-	}
-
-	function verificationPassword($id, $pass)
-	{
-		$pass = $this->pass($pass);
-
-		$query = "SELECT id FROM " . $this->tb . " WHERE password='" . $pass . "' AND id=" . intval($id) . " LIMIT 1";
-		$res = mysql_query($query);
-
-		if (mysql_num_rows($res)) {
-			return true;
-		}
-
-		return false;
-	}
-
-// MYSQL
-
 }
